5b4d6813a1322beea1bc8b55821834f840c93680
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-20 13:38:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function wakeDog(dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`;
+}
+function leashDog(dogName, dogBreed) {
+  console.log(`leash ${dogName} the ${dogBreed}`);
+  return `leash ${dogName} the ${dogBreed}`;
+}
+function walkToPark(dogName, dogBreed) {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`;
+}
+function throwFrisbee(dogName, dogBreed) {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+}
+function walkhome(dogName, dogBreed) {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return `Walk home with ${dogName} the ${dogBreed}`;
+}
+  function unleashDog(dogName, dogBreed) {
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`;
+}
+
+const routine = ["wakeDog(dogName, dogBreed) " , "leashDog(dogName, dogBreed) ", "walkToPark(dogName, dogBreed) ", "throwFrisbee(dogName, dogBreed) ","walkhome(dogName, dogBreed) ", "unleashDog(dogName, dogBreed) "];
+
+function exerciseDog(dogName,dogBreed){ 
+  for (let i=0; i<=routine.length; i++ )
+  
+  console.log (`routine[i](dogName,dogBreed)`);
+  return `routine[i](dogName,dogBreed)`;
+}
