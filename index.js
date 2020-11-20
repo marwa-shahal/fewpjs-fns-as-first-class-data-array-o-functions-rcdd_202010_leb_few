e1a6bcafb14d78cb395ddc990fe92adf537c5b3c
binding-event-8377 <marwa.al.shahal@gmail.com>
2020-11-20 14:58:55 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const routine = [wakeDog, leashDog, walkToPark,  throwFrisbee, walkHome, unleash
 function exerciseDog(dogName,dogBreed){
   let result=[];
   for (let i=0; i<routine.length; i++ )
-  routine[i](dogName,dogBreed);
+  
    result.push(routine);
    return  result;
   
